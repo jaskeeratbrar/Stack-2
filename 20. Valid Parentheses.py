7ba54cbd5a339dc5a323bf886549f9136da632c1
Jaskeerat Brar <jassibrar1999@gmail.com>
2024-10-01 16:12:05 -0700

    Update 20. Valid Parentheses.py

--- 20. Valid Parentheses.py
+++ 20. Valid Parentheses.py
@@ -4,28 +4,18 @@
 class Solution:
     def isValid(self, s: str) -> bool:
 
-        mapStore = {")":"(", "]":"[", "}":"{" }
+        mapTracker = {"]" : "[", "}" : "{", ")" : "(" }
 
         stack = []
 
         for i in s:
 
-        ##  if our string character is a closing bracket
-            if i in mapStore:
-
-
-                if stack and stack[-1] == mapStore[i]:
+            if stack and i in mapTracker:
+                if stack[-1] == mapTracker[i]:
                     stack.pop()
-                
-                ## to handle edge case, incase stack is of size 1 and string only contains
-                ## closing paranthesis as its sole character
-
                 else:
                     return False
-            
-            ## keep pushing opening paranthesis to our stack        
+
             else: stack.append(i)
         
-        ## if len is zero, it means our paranthesis was valid
         if not stack: return True
-        
